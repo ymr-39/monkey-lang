1a932d4678e706972425e1253da67bc373d8d3e7
maiyamag <maiyamag@yahoo-corp.jp>
2018-11-19 18:48:12 +0900

    add additional keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -80,11 +80,24 @@ func TestNextToken(t *testing.T) {
 		{Type: token.INT, Literal: "5"},
 		{Type: token.SEMICOLON, Literal: ";"},
 
-		// if (5 < 10) {
-		// 	return true;
-		// } else {
-		// 	return false;
-		// }
+		{Type: token.IF, Literal: "if"},
+		{Type: token.LPAREN, Literal: "("},
+		{Type: token.INT, Literal: "5"},
+		{Type: token.LT, Literal: "<"},
+		{Type: token.INT, Literal: "10"},
+		{Type: token.RPAREN, Literal: ")"},
+		{Type: token.LBRACE, Literal: "{"},
+		{Type: token.RETURN, Literal: "return"},
+		{Type: token.TRUE, Literal: "true"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.RBRACE, Literal: "}"},
+		{Type: token.ELSE, Literal: "else"},
+		{Type: token.LBRACE, Literal: "{"},
+		{Type: token.RETURN, Literal: "return"},
+		{Type: token.FALSE, Literal: "false"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.RBRACE, Literal: "}"},
+
 		{Type: token.EOF, Literal: ""},
 	}
 
