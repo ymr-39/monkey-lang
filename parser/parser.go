7e6dbc54fe0ffb2ad87a3ae4c716a855dfcd143c
maiyamag <maiyamag@yahoo-corp.jp>
2018-11-20 13:20:07 +0900

    parse let statements

---
+++ parser/parser.go
@@ -0,0 +1,88 @@
+package parser
+
+import (
+	"github.com/ymr-39/monkey-lang/ast"
+	"github.com/ymr-39/monkey-lang/lexer"
+	"github.com/ymr-39/monkey-lang/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{Statements: []ast.Statement{}}
+
+	for !p.currentTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currentToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.currentToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	if !p.currentTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) currentTokenIs(tokenType token.TokenType) bool {
+	return tokenType == p.currentToken.Type
+}
+
+func (p *Parser) peekTokenIs(tokenType token.TokenType) bool {
+	return tokenType == p.peekToken.Type
+}
+
+func (p *Parser) expectPeek(tokenType token.TokenType) bool {
+	if p.peekTokenIs(tokenType) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
